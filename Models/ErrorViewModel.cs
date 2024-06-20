4ca9dfc5bddc80fe1d21aafc0b7f8102581ff236
kimdung0910 <kimdung09102002@gmail.com>
2024-06-20 19:43:21 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TTKDBaiThiLai.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
